035456ff51e055f5d50ea64aa775a72fa090b474
zzzyh <yihu229@gmail.com>
2023-12-25 16:19:26 -0500

    added forward process

--- hyr_main.py
+++ hyr_main.py
@@ -1,23 +1,34 @@
 # This is a script for training and testing the unsupervised diffusion model
 #
-
 import numpy as np
+import torch
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_swiss_roll
+
+def forward_process(data, T, betas):
+    for t in range(T):
+        beta_t = betas[t]
+        mu = data * np.sqrt(1 - beta_t)
+        std = np.sqrt(beta_t)
+        data = mu + std * np.random.randn(data.shape[0], data.shape[1]) # data ~ N(mu, std)
+
+
 def main():
     # load 2D swiss roll dataset
     n_samples = 5000
     data, _ = make_swiss_roll(n_samples)
     data = data[:, [2, 0]]/10
     data = data * np.array([1, -1])
-    print(data)
     # plot the 2D swiss roll dataset
     plt.figure(figsize=(10, 10))
     plt.scatter(data[:, 0], data[:, 1])
     plt.xlim([-2, 2])
     plt.ylim([-2, 2])
     plt.show()
-
+    # start forward diffusion
+    T = 40
+    betas = np.linspace(1e-4, 1e-2, T)
+    forward_process(data, T, betas)
 
 
 if __name__== '__main__':
