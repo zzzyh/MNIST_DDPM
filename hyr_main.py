124073aba9a431d031a5dd866339fa3783e3e297
zzzyh <yihu229@gmail.com>
2023-12-25 00:02:37 -0500

    added swiss roll

--- hyr_main.py
+++ hyr_main.py
@@ -1,7 +1,23 @@
 # This is a script for training and testing the DDPM model on MNIST dataset.
 
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_swiss_roll
 def main():
-    pass
+    # load 2D swiss roll dataset
+    n_samples = 5000
+    data, _ = make_swiss_roll(n_samples)
+    data = data[:, [2, 0]]/10
+    data = data * np.array([1, -1])
+    print(data)
+    # plot the 2D swiss roll dataset
+    plt.figure(figsize=(10, 10))
+    plt.scatter(data[:, 0], data[:, 1])
+    plt.xlim([-2, 2])
+    plt.ylim([-2, 2])
+    plt.show()
+
+
 
 if __name__== '__main__':
     # Define the parameters
